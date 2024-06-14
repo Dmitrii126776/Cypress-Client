84073330b864546c7860ac43f9b8f456f49358e9
Dmitrii126776 <dukek67@gmail.com>
2024-06-14 17:32:45 -0400

    update cypress-e2e.yml

--- cypress.config.js
+++ cypress.config.js
@@ -31,7 +31,7 @@ module.exports = defineConfig({
         },
         setupNodeEvents(on, config) {
             // implement node event listeners here
-            require('cypress-mochawesome-reporter/plugin')(on);
+            // require('cypress-mochawesome-reporter/plugin')(on);
         },
     },
 });
