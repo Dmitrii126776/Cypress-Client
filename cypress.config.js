799f68230e95cf3e59c36ff39c5044e72faecaf4
Dmitrii126776 <dukek67@gmail.com>
2024-07-08 15:31:42 -0400

    fix animalsBoardClient.page.js

--- cypress.config.js
+++ cypress.config.js
@@ -33,7 +33,7 @@ module.exports = defineConfig({
         },
         setupNodeEvents(on, config) {
             // implement node event listeners here
-            require('cypress-mochawesome-reporter/plugin')(on);
+            // require('cypress-mochawesome-reporter/plugin')(on);
         },
     },
 });
